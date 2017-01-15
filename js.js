3f480d0ff426f2f2da1ea60b4c0f77d1c641c9a0
George Higbie <gehigbie@gmail.com>
2017-01-14 20:05:55 -0700

    fixed bug and refactored

--- js.js
+++ js.js
@@ -4,14 +4,12 @@ var numbers = [1,2,3,4,5,6,7];
 var chars = ["a", "b", "c", "d", "e", "f"];
 
 function printReverse(array){
-	var length = array.length;
-
 	for(var a = array.length-1; a > -1; a--){
 		console.log(array[a]);
-		console.log("/////////////");
 	}
 }
 
 printReverse(numbers);
+console.log("///////////////")
 printReverse(chars);
 
