30c7a23a10c97de0d25f66ddcfaf37864c5cec75
George Higbie <gehigbie@gmail.com>
2017-01-14 20:00:57 -0700

    added a printReverse() function

--- js.js
+++ js.js
@@ -1,2 +1,16 @@
 //this document was created in the command line
 
+var numbers = [1,2,3,4,5,6,7];
+var chars = ["a", "b", "c", "d", "e", "f"];
+
+function printReverse(array){
+	var length = array.length;
+
+	for(var a = array.length-1; a < 0; a--){
+		console.log(array[a]);
+	}
+}
+
+
+printReverse(numbers);
+printReverse(chars);
